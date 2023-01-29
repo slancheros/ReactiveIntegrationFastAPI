7d05636fb04ff771a2e00c7dac5cf9fe71ec61d0
slancheros <sandrallancherosg@hotmail.com>
2023-01-29 17:18:51 +0100

    Adding an iPywidgets set of examples, adding requirements.txt file

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 from json import loads
 from time import sleep
 consumer = KafkaConsumer(
-    'topic_test',
+    'topic_valencia',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
